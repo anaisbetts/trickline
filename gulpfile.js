2cb1c79205697ed7f7d1fb9eac0ffc14647249d5
Paul Betts <paul@paulbetts.org>
2015-10-30 23:31:07 -0700

    So closeeeee

--- gulpfile.js
+++ gulpfile.js
@@ -302,7 +302,7 @@ gulp.task('default', ['clean'], function (cb) {
 
 // Load tasks for web-component-tester
 // Adds tasks for `gulp test:local` and `gulp test:remote`
-require('web-component-tester').gulp.init(gulp);
+//require('web-component-tester').gulp.init(gulp);
 
 // Load custom tasks from the `tasks` directory
 try { require('require-dir')('tasks'); } catch (err) {}
