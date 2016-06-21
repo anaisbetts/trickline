e01eab7d4b0d702be11f07a2c2e129ad9fe26811
Paul Betts <paul@paulbetts.org>
2016-06-21 14:52:52 -0700

    HOLY SHIT IT WORKS

--- src/model.js
+++ src/model.js
@@ -2,27 +2,49 @@ import { Observable, Subscription, Subject } from 'rxjs/Rx';
 
 const d = require('debug')('trickline:model');
 
-function getDescriptorForProperty(target, name, descriptor) {
-  let val = undefined;
+function isObject(o) {
+  return o === Object(o);
+}
+
+function getDescriptorsForProperty(target, name, descriptor) {
+  let backingStoreName = `__${name}__`;
   
-  return Object.assign({
-    get: () => val,
+  let realProp = Object.assign({
+    get: function() { return this[backingStoreName]; },
     set: function(newVal) {
-      if (val === newVal) return;
+      console.log("Calling Setter! " + this);
+      if (this[backingStoreName] === newVal) return;
       
-      this.changing.next({sender: this, property: name});
-      val = newVal;
-      this.changed.next({sender: this, property: name});
+      console.log("Signaling changing! " + this[backingStoreName]);
+      this.changing.next({sender: this, property: name, value: this[backingStoreName]});
+      this[backingStoreName] = newVal;
+      console.log("Signaling changed! " + this[backingStoreName]);
+      this.changed.next({sender: this, property: name, value: newVal});
     }
   }, descriptor);
+  
+  let backingStoreProp = {
+    value: undefined,
+    writable: true,
+    enumerable: false,
+    configurable: false,
+  };
+  
+  let ret = {};
+  ret[name] = realProp; ret[backingStoreName] = backingStoreProp;
+  return ret;
 }
 
 export function notify(...properties) {
   return (target) => {
     for (let prop of properties) {
-      let descriptor = getDescriptorForProperty(
+      let descriptorList = getDescriptorsForProperty(
         target.prototype, prop, { configurable: true, enumerable: true });
-      Object.defineProperty(target.prototype, prop, descriptor);
+        
+      for (let k of Object.keys(descriptorList)) {
+        console.log("Defining " + k);
+        Object.defineProperty(target.prototype, k, descriptorList[k]);
+      }
     }
   };
 }
@@ -67,5 +89,87 @@ export class Model {
   
   unsubscribe() {
     this.innerDisp.unsubscribe();
+  }
+  
+  when(...props) {
+  }
+  
+  static createGetterForPropertyChain_(chain) {
+    let props = null;
+    if (Array.isArray(chain)) {
+      props = chain;
+    } else {
+      props = chain.split('.');
+    }
+  
+    return function(target) { 
+      let ret = target;
+      for (let prop of props) {
+        if (!isObject(ret) || !(prop in ret)) return {success: false};
+
+        ret = ret[prop];
+      }
+      
+      return {success: true, value: ret};
+    };
+  }
+  
+  static observableForPropertyChain_(target, chain, before=false) {
+    let props = null;
+    
+    if (Array.isArray(chain)) {
+      props = chain;
+    } else {
+      props = chain.split('.');
+    }
+    
+    /*
+    const identifier = /^[$A-Z_][0-9A-Z_$]*$/i;
+    if (props.find((x) => x.match(identifier) === null)) {
+      throw new Error("property name must be of the form 'foo.bar.baz'");
+    }
+    */
+    
+    let firstProp = props[0];
+    let start = Model.notificationForProperty_(target, firstProp, before);
+    
+    if (isObject(target) && props[0] in target) {
+      start = start.startWith({ sender: target, property: firstProp, value: target[firstProp] });
+    }
+    
+    if (props.length === 1) {
+      return start.distinctUntilChanged((x,y) => x.value === y.value);
+    }
+    
+    return start    // target.foo
+      .map((x) => {
+        console.log(`Upper change! ${x.property}`);
+        return Model.observableForPropertyChain_(x.value, props.slice(1), before)
+          .map((y) => {
+            // This is for target.foo.bar.baz, its sender will be
+            // target.foo, and its property will be bar.baz
+            console.log(`Inferior change! ${y.property}`);
+            return { sender: target, property: `${firstProp}.${y.property}`, value: y.value };
+          });
+      })
+      .switch()
+      .distinctUntilChanged((x,y) => x.value === y.value);
+  }
+
+  static notificationForProperty_(target, prop, before=false) {
+    console.log('Creating notifier for ' + prop);
+    if (!(target instanceof Model)) {
+      console.log("Returning never!");
+      return Observable.never();
+    }
+    
+    if (!(prop in target)) {
+      console.log("Not a real prop!");
+      return Observable.never();
+    }
+    
+    console.log("Returning a real thing!");
+    return target[before ? 'changing' : 'changed']
+      .filter(({property}) => prop === property);
   }
 }
--- test/model.js
+++ test/model.js
@@ -1,6 +1,9 @@
 import {Observable, Subject} from 'rxjs/Rx';
+
 import {notify, asProperty, Model} from '../src/model';
 import '../src/custom-operators';
+
+const d = require('debug')('trickline-test:model');
 
 @notify('foo', 'bar')
 class TestClass extends Model {
@@ -18,6 +21,7 @@ class TestClass extends Model {
   
   constructor() {
     super();
+    
     this.someSubject = new Subject();
   }
 }
@@ -25,10 +29,10 @@ class TestClass extends Model {
 describe('the notify attribute', function() {
   it('should notify me!', function() {
     var fixture = new TestClass();
-  
+    
     let result = Observable.merge(
-      fixture.changing.map((x) => ({ changing: true, name: x })),
-      fixture.changed.map((x) => ({ changing: false, name: x }))
+      fixture.changing.map((x) => ({ changing: true, name: x.property })),
+      fixture.changed.map((x) => ({ changing: false, name: x.property }))
     ).createCollection();
     
     expect(result.length).to.equal(0);
@@ -66,8 +70,8 @@ describe('the asProperty attribute', function() {
     expect(fixture.subjectDerived).to.equal(0);
     
     let changes = Observable.merge(
-      fixture.changing.map((x) => ({ changing: true, name: x })),
-      fixture.changed.map((x) => ({ changing: false, name: x }))
+      fixture.changing.map((x) => ({ changing: true, name: x.property })),
+      fixture.changed.map((x) => ({ changing: false, name: x.property }))
     ).createCollection();  
     
     expect(changes.length).to.equal(0);
@@ -76,5 +80,168 @@ describe('the asProperty attribute', function() {
     expect(fixture.subjectDerived).to.equal(100);
     expect(changes[0]).to.deep.equal({ changing: true, name: 'subjectDerived' });
     expect(changes[1]).to.deep.equal({ changing: false, name: 'subjectDerived' });
+  });
+});
+
+describe('the when method', function() {
+  it('should let me get props', function() {
+    let f = { bamf: 10, foo: { bar: { baz: 5 } } };
+    
+    let result = Model.createGetterForPropertyChain_('bamf');
+    expect(result(f)).to.deep.equal({success: true, value: 10});
+    
+    let result2 = Model.createGetterForPropertyChain_('foo.bar.baz');
+    expect(result2(f)).to.deep.equal({success: true, value: 5});
+    
+    let result3 = Model.createGetterForPropertyChain_('foo.bar.nothere');
+    expect(result3(f)).to.deep.equal({success: false});
+    
+    let result4 = Model.createGetterForPropertyChain_('nothere.bar.baz');
+    expect(result4(f)).to.deep.equal({success: false});
+  });
+
+  it('should notify me about props', function() {
+    let fixture = { foo: new TestClass() };
+    let inner = fixture.foo;
+    
+    let changes = Model.notificationForProperty_(inner, 'bar').createCollection();
+    expect(changes.length).to.equal(0);
+    
+    inner.bar = 5;
+    expect(changes.length).to.equal(1);
+    expect(changes[0]).to.deep.equal({sender: inner, property: 'bar', value: 5});
+    expect(Object.keys(changes[0]).length).to.equal(3);
+    
+    let changes2 = Model.notificationForProperty_(fixture, 'foo').createCollection();
+    expect(changes2.length).to.equal(0);
+    
+    changes2.foo = new TestClass();
+    expect(changes2.length).to.equal(0);
+  });
+    
+  it('should return nothing for non-models', function() {
+    let changes = Model.notificationForProperty_(5)
+      .materialize()
+      .createCollection();
+      
+    expect(changes.length).to.equal(0);
+        
+    let input = {foo: 'bar'};
+    changes = Model.notificationForProperty_(input)
+      .materialize()
+      .createCollection();
+      
+    expect(changes.length).to.equal(0);
+    
+    input.foo = 'barrr';
+    expect(changes.length).to.equal(0);
+  });
+  
+  it('should return nothing for expressions it cant actually fetch', function() {
+    var fixture = new TestClass();
+    let result = Model.observableForPropertyChain_(fixture, '__nothere').createCollection();
+    expect(result.length).to.equal(0);
+    
+    fixture.__nothere = 0;
+    expect(result.length).to.equal(0);
+  });
+  
+  it('should subscribe to a one-item expression chain', function() {
+    console.log("Startup");
+    
+    var fixture = new TestClass();
+    let result = Model.observableForPropertyChain_(fixture, 'foo').createCollection();
+    expect(result.length).to.equal(1);
+    
+    console.log("Set to 5");
+        
+    fixture.foo = 5;
+    expect(result.length).to.equal(2);
+    expect(result[1]).to.deep.equal({ sender: fixture, property: 'foo', value: 5});
+    
+    console.log("Set to 5 again");
+    
+    fixture.foo = 5;
+    expect(result.length).to.equal(2);
+    
+    console.log("Set to 7");
+    
+    fixture.foo = 7;
+    expect(result.length).to.equal(3);
+    
+    expect(result[2]).to.deep.equal({ sender: fixture, property: 'foo', value: 7});
+    expect(Object.keys(result[0]).length).to.equal(3);
+    expect(Object.keys(result[1]).length).to.equal(3);
+    expect(Object.keys(result[2]).length).to.equal(3);
+  });
+  
+  it('distinct should do what I expect it to', function() {
+    let input = [
+      { foo: 'bar', baz: 1 },
+      { foo: 'bar', baz: 2 },
+      { foo: 'bar', baz: 2 },
+      { foo: 'bar', baz: 3 },
+      { foo: 'bar', baz: 3 }
+    ];
+    
+    let result = Observable.of(...input).distinct((a,b) => a.baz === b.baz).createCollection();
+    expect(result.length).to.equal(3);
+    expect(result[0].baz).to.equal(1);
+    expect(result[1].baz).to.equal(2);
+    expect(result[2].baz).to.equal(3);
+  });
+  
+  it('switch should do what I expect', function() {
+    var input = new Subject();
+    var result = input
+      .map((x) => x.subj)
+      .switch()
+      .createCollection();
+      
+    expect(result.length).to.equal(0);
+    
+    input.next({subj: Observable.of(1,2,3).concat(Observable.never())});
+    expect(result.length).to.equal(3);
+    input.next({subj: Observable.of(4,5)});
+    expect(result.length).to.equal(5);
+  });
+  
+  it('should subscribe to a multi-item expression chain', function() {
+    var fixture = new TestClass();
+    fixture.bar = new TestClass();
+    var barFixture = fixture.bar;
+    
+    let result = Model.observableForPropertyChain_(fixture, 'bar.foo').createCollection();
+    expect(result.length).to.equal(1);
+    expect(result[0].sender).to.equal(fixture);
+    expect(result[0].property).to.equal('bar.foo');
+    expect(result[0].value).to.equal(undefined);
+    
+    
+    fixture.bar.foo = 5;
+    expect(result.length).to.equal(2);
+    expect(result[1].sender).to.equal(fixture);
+    expect(result[1].property).to.equal('bar.foo');
+    expect(result[1].value).to.equal(5);
+    
+    barFixture.foo = 8;
+    expect(result.length).to.equal(3);
+    expect(result[2].sender).to.equal(fixture);
+    expect(result[2].property).to.equal('bar.foo');
+    expect(result[2].value).to.equal(8);
+    
+    console.log("START READING HERE");
+    fixture.bar = new TestClass();
+    console.log(`New foo is ${fixture.bar.foo}`);
+    expect(result.length).to.equal(4);
+    expect(result[3].sender).to.equal(fixture);
+    expect(result[3].property).to.equal('bar.foo');
+    expect(result[3].value).to.equal(fixture.bar.foo);
+    
+    fixture.bar = 5;
+    expect(result.length).to.equal(4);
+  
+    barFixture.foo = 7;
+    expect(result.length).to.equal(4);
   });
 });
