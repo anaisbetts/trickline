d91dbac29ab168a938ac238c932a8396535894b8
Paul Betts <paul@paulbetts.org>
2016-10-20 22:27:18 -0700

    Custom Operators to TypeScript

--- src/custom-operators.js
+++
@@ -1,8 +0,0 @@
-import {Observable} from 'rxjs/Rx';
-
-Observable.prototype.createCollection = function() {
-  let ret = [];
-  this.subscribe((x) => ret.push(x));
-
-  return ret;
-};
---
+++ src/custom-operators.ts
@@ -0,0 +1,10 @@
+import {Observable} from 'rxjs/Rx';
+
+function createCollection<T>(this: Observable<T>) : Array<T> {
+  let ret : Array<T> = [];
+  this.subscribe((x) => ret.push(x));
+
+  return ret;
+}
+
+Observable.prototype['createCollection'] = createCollection;
