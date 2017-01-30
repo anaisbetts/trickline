b54ce98c6bbc357873a6284dd1b262c16333d553
Paul Betts <paul@paulbetts.org>
2017-01-29 16:46:06 -0800

    Whatever

---
+++ src/channel-list.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Channel List</h2>
+</template>
+
+<script lang="js">
+export default {
+  name: 'channel-list',
+}
+</script>
---
+++ src/message-pane.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Message Pane</h2>
+</template>
+
+<script lang="js">
+export default {
+  name: 'message-pane',
+}
+</script>
+
