275cd99a77a32e495425f70a298df9ff01e5e265
Paul Betts <paul@paulbetts.org>
2017-02-07 10:43:04 -0800

    Types not Types

--- src/models/api-call.ts
+++ src/models/api-call.ts
@@ -1,7 +1,7 @@
 import { RecursiveProxyHandler } from 'electron-remote';
 import { Observable } from 'rxjs/Observable';
 
-export function create(token?: string) {
+export function create(token?: string): any {
   const defaultParams = token ? {token} : {};
 
   return RecursiveProxyHandler.create('api', (names: Array<string>, params: Array<any>) => {
