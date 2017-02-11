e777377e0aa9506068344598c3235e747dad45ee
Paul Betts <paul@paulbetts.org>
2017-02-10 17:00:04 -0800

    Wire it up

--- src/slack-app.tsx
+++ src/slack-app.tsx
@@ -5,21 +5,63 @@ import { default as AppBar } from 'material-ui/AppBar';
 import { default as Drawer } from 'material-ui/Drawer';
 import { default as MuiThemeProvider } from 'material-ui/styles/MuiThemeProvider';
 
+import { Observable } from 'rxjs/Observable';
+import { ChannelBase, UsersCounts } from './models/api-shapes';
+import { createApi } from './models/api-call';
+
 import { Action } from './action';
 import { SimpleView } from './view';
 import { asProperty, Model } from './model';
+import { InMemorySparseMap, Updatable } from './sparse-map';
 
 export interface SlackAppState {
   drawerOpen: boolean;
 }
 
+export class Store {
+  joinedChannels: SparseMap<string, ChannelBase>;
+  api: any;
+
+  constructor(token?: string) {
+    this.api = createApi(token);
+    this.joinedChannels = new InMemorySparseMap();
+  }
+
+  async update(): Promise<void> {
+    let result: UsersCounts = await this.api.users.counts().toPromise();
+
+    result.channels.forEach(c => {
+      let updater = new Updatable(() => this.api.channels.info({channel: c.id}).map(x => x.channel as ChannelBase));
+      updater.playOnto(Observable.of(c));
+
+      this.joinedChannels.setDirect(c.id, updater);
+    });
+
+    result.groups.forEach(c => {
+      let updater = new Updatable(() => this.api.groups.info({channel: c.id}).map(x => x.group as ChannelBase));
+      updater.playOnto(Observable.of(c));
+
+      this.joinedChannels.setDirect(c.id, updater);
+    });
+
+    result.ims.forEach(c => {
+      let updater = new Updatable(() => this.api.im.info({channel: c.id}).map(x => x.im as ChannelBase));
+      updater.playOnto(Observable.of(c));
+
+      this.joinedChannels.setDirect(c.id, updater);
+    });
+  }
+}
+
 export class SimpleViewModel extends Model {
   toggleDrawer: Action<boolean>;
+  store: Store;
 
   constructor() {
     super();
 
     let isOpen = false;
+    this.store = new Store(localStorage.getItem('token'));
     this.toggleDrawer = Action.create(() => isOpen = !isOpen, false);
   }
 
