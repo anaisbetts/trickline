7c55ff18a6b00098753b9d7f6bb1a0ae95307106
Paul Betts <paul@paulbetts.org>
2017-02-17 18:28:47 -0800

    Write a lot of ugly code to avoid work in View creation

--- src/view.ts
+++ src/view.ts
@@ -21,11 +21,11 @@ export abstract class Lifecycle<P, S> {
   willUnmount: Observable<boolean>;
 
   static attach<P, S>(target: React.Component<P, S>): Lifecycle<P, S> {
-    return new ReactLifecycle(target);
+    return new AttachedReactLifecycle(target);
   }
 }
 
-class ReactLifecycle<P, S> extends Lifecycle<P, S> {
+class AttachedReactLifecycle<P, S> extends Lifecycle<P, S> {
   private readonly willMountSubj: AsyncSubject<boolean>;
   private readonly didMountSubj: AsyncSubject<boolean>;
   private readonly willReceivePropsSubj: Subject<P>;
@@ -49,17 +49,33 @@ class ReactLifecycle<P, S> extends Lifecycle<P, S> {
 
     for (const name of ['willMount', 'didMount', 'willUnmount']) {
       const subj = this[name + 'Subj'] = new AsyncSubject();
-      target[ReactLifecycle.reactMethodName(name)] = () => { subj.next(true); subj.complete(); };
+      target[AttachedReactLifecycle.reactMethodName(name)] = () => { subj.next(true); subj.complete(); };
     }
 
     for (const name of ['willUpdate', 'didUpdate']) {
       const subj = this[name + 'Subj'] = Subject.create();
-      target[ReactLifecycle.reactMethodName(name)] = (p: P, s: S) => subj.next({props: p, state: s});
+      target[AttachedReactLifecycle.reactMethodName(name)] = (p: P, s: S) => subj.next({props: p, state: s});
     }
 
     const ps = this.willReceivePropsSubj = Subject.create();
     target['componentWillReceiveProps'] = (props: P) => ps.next(props);
   }
+}
+
+class ReactLifecycle<P, S> extends Lifecycle<P, S> {
+  willMountSubj: AsyncSubject<boolean>;
+  didMountSubj: AsyncSubject<boolean>;
+  willReceivePropsSubj: Subject<P>;
+  willUpdateSubj: Subject<{props: P, state: S}>;
+  didUpdateSubj: Subject<{props: P, state: S}>;
+  willUnmountSubj: AsyncSubject<boolean>;
+
+  get willMount() { return (this.willMountSubj = this.willMountSubj || new AsyncSubject()); }
+  get didMount() { return (this.didMountSubj = this.didMountSubj || new AsyncSubject()); }
+  get willReceiveProps() { return (this.willReceivePropsSubj = this.willReceivePropsSubj || new Subject()); }
+  get willUpdate() { return (this.willUpdateSubj = this.willUpdateSubj || new Subject()); }
+  get didUpdate() { return (this.didUpdateSubj = this.didUpdateSubj || new Subject()); }
+  get willUnmount() { return (this.willUnmountSubj = this.willUnmountSubj || new AsyncSubject()); }
 }
 
 export interface HasViewModel<T extends Model> {
@@ -76,7 +92,7 @@ export abstract class View<T extends Model, P extends HasViewModel<T>>
     super(props, context);
     if (props) this.viewModel = props.viewModel;
 
-    this.lifecycle = Lifecycle.attach(this);
+    this.lifecycle = new ReactLifecycle<P, null>();
 
     this.lifecycle.didMount
       .flatMap(() => this.viewModel.changed)
@@ -84,6 +100,39 @@ export abstract class View<T extends Model, P extends HasViewModel<T>>
       .subscribe(() => this.forceUpdate());
 
     this.lifecycle.willUnmount.subscribe(() => { if (this.viewModel) this.viewModel.unsubscribe(); });
+  }
+
+  componentWillMount() {
+    if (!this.lifecycle.willMountSubj) return;
+    this.lifecycle.willMountSubj.next(true);
+    this.lifecycle.willMountSubj.complete();
+  }
+
+  componentDidMount() {
+    if (!this.lifecycle.didMountSubj) return;
+    this.lifecycle.didMountSubj.next(true);
+    this.lifecycle.didMountSubj.complete();
+  }
+
+  componentWillReceiveProps(props: P) {
+    if (!this.lifecycle.willReceivePropsSubj) return;
+    this.lifecycle.willReceivePropsSubj.next(props);
+  }
+
+  componentWillUpdate(props: P) {
+    if (!this.lifecycle.willUpdateSubj) return;
+    this.lifecycle.willUpdateSubj.next({props, state: {}});
+  }
+
+  componentDidUpdate(props: P) {
+    if (!this.lifecycle.didUpdateSubj) return;
+    this.lifecycle.didUpdateSubj.next({props, state: {}});
+  }
+
+  componentWillUnmount() {
+    if (!this.lifecycle.willUnmountSubj) return;
+    this.lifecycle.willUnmountSubj.next(true);
+    this.lifecycle.willUnmountSubj.complete();
   }
 }
 
