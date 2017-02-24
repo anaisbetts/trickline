6bcd934742c52eac36a0a52b6a04d703d476fa74
Charlie Hess <charlie@slack-corp.com>
2017-02-23 18:20:12 -0800

    Put a little memory popover in the corner for us to reference.

---
+++ src/memory-popover.tsx
@@ -0,0 +1,76 @@
+// tslint:disable-next-line:no-unused-variable
+import * as React from 'react';
+
+import RaisedButton from 'material-ui/RaisedButton';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import DeveloperBoard from 'material-ui/svg-icons/hardware/developer-board';
+
+export interface MemoryPopoverProps {
+}
+
+export interface MemoryPopoverState {
+  open: boolean;
+  anchorElement?: EventTarget;
+}
+
+export class MemoryPopover extends React.Component<MemoryPopoverProps, MemoryPopoverState> {
+
+  constructor(props: MemoryPopoverProps) {
+    super(props);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleTouchTap = (event: Event) => {
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorElement: event.currentTarget
+    });
+  }
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false
+    });
+  }
+
+  render() {
+    const { open, anchorElement } = this.state;
+
+    const memoryUsageInMB = open ?
+      `${Math.round(process.getProcessMemoryInfo().privateBytes / 1024)} MB` : null;
+
+    const popoverStyle = {
+      position: 'absolute',
+      right: '0px',
+      bottom: '0px'
+    };
+
+    return (
+      <div style={popoverStyle}>
+        <RaisedButton
+          onTouchTap={this.handleTouchTap}
+          icon={<DeveloperBoard />}
+          label='Memory Usage'
+        />
+        <Popover
+          open={open}
+          anchorEl={anchorElement}
+          anchorOrigin={{horizontal: 'middle', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'middle', vertical: 'top'}}
+          onRequestClose={this.handleRequestClose}
+        >
+          <Menu>
+            <MenuItem primaryText={memoryUsageInMB} disabled/>
+          </Menu>
+        </Popover>
+      </div>
+    );
+  }
+}
--- src/slack-app.tsx
+++ src/slack-app.tsx
@@ -14,6 +14,7 @@ import { fromObservable, Model } from './lib/model';
 import { Store } from './lib/store';
 
 import { ChannelListViewModel, ChannelListView } from './channel-list';
+import { MemoryPopover } from './memory-popover';
 
 import './lib/standard-operators';
 
@@ -55,20 +56,27 @@ export class SlackApp extends SimpleView<SlackAppModel> {
   render() {
     const vm = this.viewModel;
     const shouldShift = vm.isOpen && window.outerWidth > window.outerHeight;
+    const containerStyle = {
+      marginLeft: shouldShift ? '258px' : '0px',
+      transition: 'margin-left: 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms'
+    };
+
     const channelListView = vm.isOpen ?
       <ChannelListView viewModel={new ChannelListViewModel(vm.store)} /> :
       null;
 
-    return <MuiThemeProvider>
-      <div style={{marginLeft: shouldShift ? '258px' : '0px', transition: 'margin-left: 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms'}}>
-        <AppBar title='Trickline' onLeftIconButtonTouchTap={vm.toggleDrawer.bind()} zDepth={2}/>
+    return (
+      <MuiThemeProvider>
+        <div style={containerStyle}>
+          <AppBar title='Trickline' onLeftIconButtonTouchTap={vm.toggleDrawer.bind()} zDepth={2}/>
 
-        <Drawer open={vm.isOpen} zDepth={1}>
-          {channelListView}
-        </Drawer>
+          <Drawer open={vm.isOpen} zDepth={1}>
+            {channelListView}
+          </Drawer>
 
-        <p>I am the main content</p>
-      </div>
-    </MuiThemeProvider>;
+          <MemoryPopover />
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
