47ab0198147fac0cd2ca50b738240f424c008fcf
Paul Betts <paul@paulbetts.org>
2017-02-27 15:59:04 -0800

    Start to work on typed versions of getValue et al

---
+++ src/lib/when.ts
@@ -0,0 +1,86 @@
+import * as LRU from 'lru-cache';
+import {Updatable} from './sparse-map';
+
+const proxyCache = new LRU({length: 64});
+
+function isObject(o: any): Boolean {
+  return o === Object(o);
+}
+
+const EMPTY_FN = () => {};
+export class SelfDescribingProxyHandler {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  get(target: any, name: string) {
+    return SelfDescribingProxyHandler.create(`${this.name}.${name}`);
+  }
+
+  apply() {
+    return this.name;
+  }
+
+  static create(name='') {
+    let ret = proxyCache.get(name);
+    if (ret) return ret;
+
+    ret = new Proxy(EMPTY_FN, new SelfDescribingProxyHandler(name));
+    proxyCache.set(name, ret);
+    return ret;
+  }
+}
+
+export function functionToPropertyChain(chain: Function): Array<string> {
+  let input = SelfDescribingProxyHandler.create();
+  let result: Function = chain(input);
+
+  let ret: string = result();
+  return ret.substring(1).split('.');
+}
+
+const didntWork = { failed: true };
+export function fetchValueForPropertyChain(target: any, chain: Array<string>): { result?: any, failed: boolean } {
+  let current = target;
+  if (current instanceof Updatable && chain[0] !== 'value') {
+    try {
+      current = current.value;
+    } catch (_e) {
+      return didntWork;
+    }
+  }
+
+  for (let i = 0; i < chain.length; i++) {
+    try {
+      current = current[chain[i]];
+    } catch (_e) {
+      return didntWork;
+    }
+
+    if (current === undefined) return didntWork;
+
+    // NB: Current is a non-object; if we're at the end of the chain, we
+    // should return it, if we're not, we're in an error state and should
+    // bail
+    if (!isObject(current))  {
+      return (i === chain.length - 1) ? { result: current, failed: false} : didntWork;
+    }
+
+    if (current instanceof Updatable && chain[i + 1] !== 'value') {
+      try {
+        current = current.value;
+      } catch (_e) {
+        return didntWork;
+      }
+    }
+  }
+
+  return { result: current, failed: false };
+}
+
+export function getValue<T, TRet>(target: T, accessor: ((x: T) => TRet)): { result?: TRet, failed: boolean } {
+  const propChain = functionToPropertyChain(accessor);
+  return fetchValueForPropertyChain(target, propChain);
+}
---
+++ test/lib/when-spec.ts
@@ -0,0 +1,72 @@
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { Updatable } from '../../src/lib/sparse-map';
+import { getValue } from '../../src/lib/when';
+
+import {expect, TestClass} from '../support';
+
+describe('the getValue method', function() {
+  it ('should fetch simple values', function() {
+    let fixture = new TestClass();
+
+    fixture.bar = 4;
+    expect(getValue(fixture, f => f.bar).result).to.equal(4);
+
+    fixture.bar = 10;
+    expect(getValue(fixture, f => f.bar).result).to.equal(10);
+  });
+
+  it ('should fetch through Updatable values', function() {
+    let fixture = new TestClass();
+    expect(getValue(fixture, f => f.updatableFoo).result).to.equal(6);
+
+    fixture.updatableFoo.playOnto(Observable.of(10));
+    expect(getValue(fixture, f => f.updatableFoo).result).to.equal(10);
+  });
+
+  it ('should fetch through Updatable values even when explicitly requested', function() {
+    let fixture = new TestClass();
+    expect(getValue(fixture, f => f.updatableFoo.value).result).to.equal(6);
+
+    fixture.updatableFoo.playOnto(Observable.of(10));
+    expect(getValue(fixture, f => f.updatableFoo.value).result).to.equal(10);
+  });
+
+  it ('should fetch through Updatable when its the first one', function() {
+    let fixture = new Updatable(() => Observable.of(new TestClass));
+
+    expect(getValue(fixture, f => f.updatableFoo).result).to.equal(6);
+  });
+
+  it ('should fetch through Updatable when its the first one even when explicitly requested', function() {
+    let fixture = new Updatable(() => Observable.of(new TestClass));
+
+    expect(getValue(fixture, f => f.value.updatableFoo).result).to.equal(6);
+  });
+
+  it ('should fail if it cant walk the entire property chain', function() {
+    let fixture = new TestClass();
+    let { result, failed } = getValue(fixture, f => f.blart.boop.bop);
+
+    expect(failed).to.be.ok;
+    expect(result).to.equal(undefined);
+  });
+
+  it ('should fail if walking the chain throws', function() {
+    let fixture = new TestClass();
+    let { result, failed } = getValue(fixture, f => f.explodingProperty.bar);
+
+    expect(failed).to.be.ok;
+    expect(result).to.equal(undefined);
+  });
+
+  it ('should fail if walking the chain throws in an Updatable', function() {
+    let fixture = new TestClass();
+    fixture.updatableFoo.playOnto(Observable.throw(new Error('die')));
+
+    let { result, failed } = getValue(fixture, f => f.updatableFoo);
+
+    expect(failed).to.be.ok;
+    expect(result).to.equal(undefined);
+  });
+});
