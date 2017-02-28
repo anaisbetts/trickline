eadc58b172283228c2cfc84dc013645e99a5191a
Charlie Hess <charlie@slack-corp.com>
2017-02-27 16:47:03 -0800

    Declare more ambient modules.

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,3 +1,8 @@
+declare module 'electron-compile';
+declare module 'electron-debug';
+declare module 'electron-devtools-installer';
+declare module 'electron-devtools-installer';
+declare module 'electron-remote';
+declare module 'dotenv';
 declare module 'material-ui/*';
 declare module 'react-virtualized';
-declare module 'electron-remote';
