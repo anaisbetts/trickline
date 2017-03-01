35fac9b19f2893a03eda9e182874ae03decd0c70
Paul Betts <paul@paulbetts.org>
2017-02-28 23:51:44 -0800

    API now has an escape hatch to get the token

--- src/lib/models/api-call.ts
+++ src/lib/models/api-call.ts
@@ -19,6 +19,8 @@ export function createApi(token?: string): any {
   const defaultParams = token ? {token} : {};
 
   return RecursiveProxyHandler.create('api', (names: Array<string>, params: Array<any>) => {
+    if (names.length === 1 && names[0] === 'token') return defaultParams.token;
+
     const p = Object.assign({}, params[0], defaultParams);
 
     return Observable.ajax.post(`https://slack.com/api/${names.slice(1).join('.')}`, p)
