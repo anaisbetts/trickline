51bff79e62e608227b153c71c8dd0cff9f8b42ee
Paul Betts <paul@paulbetts.org>
2017-03-01 14:52:22 -0800

    Capture changes to users

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,14 @@
 import { Observable } from 'rxjs/Observable';
 
-import { InMemorySparseMap, SparseMap } from './sparse-map';
-import { Updatable } from './updatable';
-import { Api, createApi } from './models/api-call';
-import { Channel, ChannelBase, Group, DirectMessage, UsersCounts, User } from './models/api-shapes';
-import { asyncMap } from './promise-extras';
+import { InMemorySparseMap, SparseMap } from './lib/sparse-map';
+import { Updatable } from './lib/updatable';
+import { Api, createApi } from './lib/models/api-call';
+import { Channel, ChannelBase, Group, DirectMessage, Message, UsersCounts, User } from './lib/models/api-shapes';
+import { EventType } from './lib/models/event-type';
+import { asyncMap } from './lib/promise-extras';
 
-import './standard-operators';
+import './lib/standard-operators';
+import 'rxjs/Observable/dom/webSocket';
 
 export type ChannelList = Array<Updatable<ChannelBase>>;
 
@@ -16,6 +18,7 @@ export class Store {
   channels: SparseMap<string, ChannelBase>;
   users: SparseMap<string, User>;
   joinedChannels: Updatable<ChannelList>;
+  events: SparseMap<EventType, Message>;
 
   constructor(tokenList: string[] = []) {
     this.api = tokenList.map(x => createApi(x));
@@ -29,6 +32,23 @@ export class Store {
     });
 
     this.joinedChannels = new Updatable<ChannelList>(() => Observable.of([]));
+
+    this.events = new InMemorySparseMap<EventType, Message>();
+    this.events.listen('user_change')
+      .do(({user}) => console.log("Updating a user!!! " + JSON.stringify(user)))
+      .subscribe(({user}) => this.users.listen(user.id).playOnto(Observable.of(user)));
+
+    this.connectToRtm()
+      .groupBy(x => x.type)
+      .publish().refCount()
+      .retry()
+      .subscribe(x => this.events.listen(x.key).playOnto(x));
+  }
+
+  connectToRtm(): Observable<Message> {
+    return Observable.merge(
+      ...this.api.map(x => this.createRtmConnection(x))
+    );
   }
 
   async fetchInitialChannelList(): Promise<void> {
@@ -40,7 +60,7 @@ export class Store {
     this.joinedChannels.next(allJoinedChannels);
   }
 
-  async fetchSingleInitialChannelList(api: Api): Promise<ChannelList> {
+  private async fetchSingleInitialChannelList(api: Api): Promise<ChannelList> {
     const joinedChannels: ChannelList = [];
 
     const result: UsersCounts = await api.users.counts({ simple_unreads: true }).toPromise();
@@ -69,5 +89,11 @@ export class Store {
     updater.playOnto(Observable.of(channel));
     this.channels.setDirect(channel.id, updater);
     return updater;
+  }
+
+  private createRtmConnection(api: Api): Observable<Message> {
+    return api.rtm.connect()
+      .flatMap(({url}) => Observable.webSocket(url))
+      .map(msg => { msg.api = api; return msg; });
   }
 }
