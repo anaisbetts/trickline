9f6b6d589f0f5da0e8839a4335cd0d893412be7b
Charlie Hess <charlie@slack-corp.com>
2017-03-03 13:59:45 -0800

    Clicking a DM should clear the topic & members tabs.

--- src/channel-header.tsx
+++ src/channel-header.tsx
@@ -6,16 +6,18 @@ import { Tab } from 'material-ui/Tabs';
 import { Action } from './lib/action';
 import { Channel, ChannelBase } from './lib/models/api-shapes';
 import { ChannelListViewModel } from './channel-list';
+import { isDM } from './channel-utils';
 import { Model, fromObservable } from './lib/model';
 import { SimpleView } from './lib/view';
 import { Store } from './store';
 import { when } from './lib/when';
+import { Observable } from 'rxjs/Observable';
 
 export class ChannelHeaderViewModel extends Model {
   @fromObservable selectedChannel: ChannelBase;
   @fromObservable channelInfo: Channel;
   @fromObservable members: Array<string>;
-  @fromObservable topic: { value: string};
+  @fromObservable topic: { value: string };
   @fromObservable isDrawerOpen: boolean;
   toggleDrawer: Action<boolean>;
 
@@ -31,21 +33,20 @@ export class ChannelHeaderViewModel extends Model {
 
     when(this, x => x.selectedChannel)
       .filter(c => !!c)
-      .switchMap(c => this.store.channels.listen(c.id, c.api))
-      .do(x => console.log(`New ChannelInfo! ${JSON.stringify(x)}`))
+      .flatMap(c => isDM(c) ? Observable.of(null) : this.store.channels.listen(c.id, c.api))
       .toProperty(this, 'channelInfo');
 
     // NB: This works but it's too damn clever
     this.innerDisp.add(when(this, x => x.channelInfo)
-      .filter(x => x && !x.topic)
-      .subscribe(x => this.store.updateChannelToLatest(x.id, x.api));
+      .filter(x => !!x)
+      .subscribe(x => this.store.updateChannelToLatest(x.id, x.api)));
 
     when(this, x => x.channelInfo.members)
       .startWith([])
       .toProperty(this, 'members');
 
     when(this, x => x.channelInfo.topic)
-      .startWith({value: ''})
+      .startWith({ value: '' })
       .toProperty(this, 'topic');
   }
 }
