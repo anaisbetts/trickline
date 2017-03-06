d395418fe96267c2515eb6e8bda948d5d6535640
Paul Betts <paul@paulbetts.org>
2017-03-05 22:46:13 -0800

    Try to take a screenshot if Trickline perf measurement fails to complete

---
+++ script/run-perf-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT="$( $SCRIPTDIR/.. )"
+
+if [ -n "$SURF_ARTIFACT_DIR" ]; then
+  SURF_ARTIFACT_DIR="$( $ROOT )"
+fi
+
+TRICKLINE_BIN="$( $ROOT/node_modules/.bin/ts-node ./script/find-trickline-exe.ts)"
+TRICKLINE_HEAPSHOT_AND_BAIL=1 $ROOT/node_modules/.bin/timeout -t 30s -- $ROOT/node_modules/.bin/xvfb-maybe $TRICKLINE_BIN
+
+if [$? != 0]; then
+  import -window root $SURF_ARTIFACT_DIR/screenshot.png
+  exit 1
+fi
+
+set -e
+
+$ROOT/node_modules/.bin/bigrig -f $SURF_ARTIFACT_DIR/trickline-timeline*.json > $SURF_ARTIFACT_DIR/bigrig.json
+$ROOT/node_modules/.bin/bigrig -f $SURF_ARTIFACT_DIR/trickline-timeline*.json --pretty-print
