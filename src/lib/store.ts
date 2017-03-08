6a259dcac79f74ff5b6612c5a3efe996e4fd1e5e
Paul Betts <paul@paulbetts.org>
2017-03-08 14:55:26 -0800

    Write the most naive store we can create

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,8 +1,9 @@
-import { SparseMap } from './sparse-map';
+import { SparseMap, InMemorySparseMap } from './sparse-map';
 import { Updatable } from './updatable';
-import { Api } from './models/slack-api';
-import { ChannelBase, Message, User } from './models/api-shapes';
+import { Api, createApi, infoApiForChannel } from './models/slack-api';
+import { ChannelBase, Message, User, UsersCounts } from './models/api-shapes';
 import { EventType } from './models/event-type';
+import { asyncMap } from './promise-extras';
 
 import './lib/standard-operators';
 import 'rxjs/add/observable/dom/webSocket';
@@ -19,4 +20,61 @@ export interface Store {
   keyValueStore: SparseMap<string, any>;
 
   fetchInitialChannelList(): Promise<void>;
+}
+
+export class NaiveStore implements Store {
+  api: Api[];
+
+  channels: SparseMap<string, ChannelBase>;
+  users: SparseMap<string, User>;
+  events: SparseMap<EventType, Message>;
+  joinedChannels: Updatable<ChannelList>;
+  keyValueStore: SparseMap<string, any>;
+
+  constructor(tokenList: string[] = []) {
+    this.api = tokenList.map(x => createApi(x));
+
+    this.channels = new InMemorySparseMap((id: string, api: Api) => infoApiForChannel(id, api).toPromise(), 'merge');
+    this.users = new InMemorySparseMap((id: string, api: Api) => api.users.info({id}).toPromise(), 'merge');
+    this.events = new InMemorySparseMap<EventType, Message>();
+    this.joinedChannels = new Updatable<ChannelList>();
+    this.keyValueStore = new InMemorySparseMap<string, any>();
+  }
+
+  async fetchInitialChannelList(): Promise<void> {
+    const results = await asyncMap(this.api, (api) => this.fetchSingleInitialChannelList(api));
+
+    const allJoinedChannels = Array.from(results.values())
+      .reduce((acc, x) => acc.concat(x), []);
+
+    this.joinedChannels.next(allJoinedChannels);
+  }
+
+  private makeUpdatableForModel(model: ChannelBase & Api, api: Api) {
+    model.api = api;
+
+    const updater = this.channels.listen(model.id, api);
+    updater.next(model);
+    return updater;
+  }
+
+  private async fetchSingleInitialChannelList(api: Api): Promise<ChannelList> {
+    const joinedChannels: ChannelList = [];
+
+    const result: UsersCounts = await api.users.counts({ simple_unreads: true }).toPromise();
+
+    result.channels.forEach((c) => {
+      joinedChannels.push(this.makeUpdatableForModel(c, api));
+    });
+
+    result.groups.forEach((g) => {
+      joinedChannels.push(this.makeUpdatableForModel(g, api));
+    });
+
+    result.ims.forEach((dm) => {
+      joinedChannels.push(this.makeUpdatableForModel(dm, api));
+    });
+
+    return joinedChannels;
+  }
 }
