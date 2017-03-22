fe3343db86ff5279f1ef154aee33cca6e428f4a8
Paul Betts <paul@paulbetts.org>
2017-03-21 19:42:55 -0700

    lol

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/index.web.tsx",
+  entry: "./src/index.tsx",
   output: {
     filename: "bundle.js",
     path: path.join(__dirname, "out", "web")
