1b6ccd21c60941540957088e967c60099a6c5d9c
Paul Betts <paul@paulbetts.org>
2017-03-30 13:37:56 -0700

    Webpack it

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,72 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "out", "web")
+module.exports = [
+  {
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.js",
+      path: path.join(__dirname, "out", "web")
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
+    ],
+
+    module: {
+      rules: [
+        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+        { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      ]
+    },
   },
+  {
+    entry: "./src/serviceworker.ts",
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+    output: {
+      filename: "sw.js",
+      path: path.join(__dirname, "out", "web")
+    },
 
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
 
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-  ],
+    resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
 
-  module: {
-    rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
+    ],
 
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-    ]
-  },
-};
+    module: {
+      rules: [
+        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+        { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      ]
+    },
+  }
+];
