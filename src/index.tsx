7f85816f06eb2b21c5ba5541cd268ecf1cc7dfe8
Paul Betts <paul@paulbetts.org>
2017-03-30 13:38:05 -0700

    Register the worker

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,11 @@ const injectTapEventPlugin = require('react-tap-event-plugin');
 
 injectTapEventPlugin();
 
+if (window.location.href.match(/^http/i) && !module.hot) {
+  navigator.serviceWorker.register('./sw.js')
+    .catch((e) => console.error(`Failed to register service worker`, e));
+}
+
 let render = () => {
   const SlackApp = require('./slack-app').SlackApp;
   ReactDOM.render(<AppContainer><SlackApp /></AppContainer>, document.getElementById('app'));
