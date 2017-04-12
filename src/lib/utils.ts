4cbec0b06ad7f1f2e02eeab16b3e6b7c972b4316
Paul Betts <paul@paulbetts.org>
2017-04-12 14:34:03 +0200

    Check to see if we're in a test runner via dodgy af means

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,10 @@
 export type Pair<K, V> = { Key: K, Value: V };
 
-export function captureStack() {
+export function captureStack(): string {
   try { throw new Error(); } catch (e) { return e.stack; }
+}
+
+export function detectTestRunner() {
+  let stack = captureStack();
+  return stack.split('\n').find(x => !!x.match(/[\\\/](enzyme|mocha)[\\\/]/i)) !== null;
 }
